0ea39491e7f5caa9f96b41679943ff9f29a3b326
Eric Grant <senatorericgrant@gmail.com>
2022-04-08 00:47:30 +0700

    Initial Commit

---
+++ MyChecklists/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyChecklists
+//
+//  Created by Eric Grant on 08.04.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
