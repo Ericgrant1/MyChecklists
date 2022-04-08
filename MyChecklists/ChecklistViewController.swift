df7e5c6bb87f0dbe3df0378e67b73e8807c67b58
Eric Grant <senatorericgrant@gmail.com>
2022-04-08 19:47:22 +0700

    fix: handle checkmarks

--- MyChecklists/ChecklistViewController.swift
+++ MyChecklists/ChecklistViewController.swift
@@ -8,6 +8,18 @@
 import UIKit
 
 class ChecklistViewController: UITableViewController  {
+    
+    var row0checked = false
+    var row1checked = false
+    var row2checked = false
+    var row3checked = false
+    var row4checked = false
+    
+    let row0text = "Walk the dog"
+    let row1text = "Brush teeth"
+    let row2text = "Learn iOS development"
+    let row3text = "Soccer practice"
+    let row4text = "Eat ice cream"
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,7 +31,7 @@ class ChecklistViewController: UITableViewController  {
         _ tableView: UITableView,
         numberOfRowsInSection section: Int
     ) -> Int {
-        return 100
+        return 5
     }
     
     override func tableView(
@@ -32,16 +44,16 @@ class ChecklistViewController: UITableViewController  {
         
         let label = cell.viewWithTag(1000) as! UILabel
         
-        if indexPath.row % 5 == 0 {
-            label.text = "Walk the dog"
-        } else if indexPath.row % 5 == 1 {
-            label.text = "Read the book"
-        } else if indexPath.row % 5 == 2 {
-            label.text = "Learn iOS development"
-        } else if indexPath.row % 5 == 3 {
-            label.text = "Programming practice"
-        } else if indexPath.row % 5 == 4 {
-            label.text = "Eat ice cream"
+        if indexPath.row == 0 {
+            label.text = row0text
+        } else if indexPath.row == 1 {
+            label.text = row1text
+        } else if indexPath.row == 2 {
+            label.text = row2text
+        } else if indexPath.row == 3 {
+            label.text = row3text
+        } else if indexPath.row == 4 {
+            label.text = row4text
         }
         
         return cell
@@ -53,10 +65,41 @@ class ChecklistViewController: UITableViewController  {
         didSelectRowAt indexPath: IndexPath
     ) {
         if let cell = tableView.cellForRow(at: indexPath) {
-            if cell.accessoryType == .none {
-                cell.accessoryType = .checkmark
-            } else {
-                cell.accessoryType = .none
+            if indexPath.row == 0 {
+                row0checked = !row0checked
+                if row0checked {
+                    cell.accessoryType = .checkmark
+                } else {
+                    cell.accessoryType = .none
+                }
+            } else if indexPath.row == 1 {
+                row1checked = !row1checked
+                if row1checked {
+                    cell.accessoryType = .checkmark
+                } else {
+                    cell.accessoryType = .none
+                }
+            } else if indexPath.row == 2 {
+                row2checked = !row2checked
+                if row2checked {
+                    cell.accessoryType = .checkmark
+                } else {
+                    cell.accessoryType = .none
+                }
+            } else if indexPath.row == 3 {
+                row3checked = !row3checked
+                if row3checked {
+                    cell.accessoryType = .checkmark
+                } else {
+                    cell.accessoryType = .none
+                }
+            } else if indexPath.row == 4 {
+                row4checked = !row4checked
+                if row4checked {
+                    cell.accessoryType = .checkmark
+                } else {
+                    cell.accessoryType = .none
+                }
             }
         }
         tableView.deselectRow(at: indexPath, animated: true)
