b621557ee1ea6b2eddfd2cfc07ea91e555a20102
Eric Grant <senatorericgrant@gmail.com>
2022-04-08 12:56:41 +0700

    feat: put row data into the cells

--- MyChecklists/ChecklistViewController.swift
+++ MyChecklists/ChecklistViewController.swift
@@ -19,7 +19,7 @@ class ChecklistViewController: UITableViewController  {
         _ tableView: UITableView,
         numberOfRowsInSection section: Int
     ) -> Int {
-        return 5
+        return 100
     }
     
     override func tableView(
@@ -29,6 +29,21 @@ class ChecklistViewController: UITableViewController  {
         let cell = tableView.dequeueReusableCell(
             withIdentifier: "ChecklistItem",
             for: indexPath)
+        
+        let label = cell.viewWithTag(1000) as! UILabel
+        
+        if indexPath.row % 5 == 0 {
+            label.text = "Walk the dog"
+        } else if indexPath.row % 5 == 1 {
+            label.text = "Read the book"
+        } else if indexPath.row % 5 == 2 {
+            label.text = "Learn iOS development"
+        } else if indexPath.row % 5 == 3 {
+            label.text = "Programming practice"
+        } else if indexPath.row % 5 == 4 {
+            label.text = "Eat ice cream"
+        }
+        
         return cell
     }
 }
