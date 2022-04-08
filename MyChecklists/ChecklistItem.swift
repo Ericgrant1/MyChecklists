991d4497ed6e70fb24fd995ce623c7b80ab50022
Eric Grant <senatorericgrant@gmail.com>
2022-04-09 00:54:46 +0700

    feat: add new object ChecklIstItem

---
+++ MyChecklists/ChecklistItem.swift
@@ -0,0 +1,13 @@
+//
+//  ChecklistItem.swift
+//  MyChecklists
+//
+//  Created by Eric Grant on 09.04.2022.
+//
+
+import Foundation
+
+class CheckListItem {
+    var text = ""
+    var checked = false
+}
