6ffb6bbc4935e8e262e9e1cb4f287daf8265beb4
Eric Grant <senatorericgrant@gmail.com>
2022-04-11 21:14:03 +0700

    feat: customize navigation bar

---
+++ MyChecklists/AddItemViewController.swift
@@ -0,0 +1,17 @@
+//
+//  AddItemViewController.swift
+//  MyChecklists
+//
+//  Created by Eric Grant on 11.04.2022.
+//
+
+import UIKit
+
+class AddItemViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        navigationItem.largeTitleDisplayMode = .never
+    }
+}
