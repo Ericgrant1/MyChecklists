b053e7ee15b0a343d1cb1ddad6b45e6cdd248644
Eric Grant <senatorericgrant@gmail.com>
2022-04-18 00:20:23 +0700

    feat: display data in table view

--- MyChecklists/AllListsViewController.swift
+++ MyChecklists/AllListsViewController.swift
@@ -37,7 +37,7 @@ class AllListsViewController: UITableViewController {
         _ tableView: UITableView,
         numberOfRowsInSection section: Int
     ) -> Int {
-        return 3
+        return lists.count
     }
     
     override func tableView(
@@ -48,8 +48,11 @@ class AllListsViewController: UITableViewController {
             withIdentifier: cellIdentifier,
             for: indexPath)
         var content = cell.defaultContentConfiguration()
-        content.text = "List \(indexPath.row)"
+        let checkList = lists[indexPath.row]
+        content.text = checkList.name
         cell.contentConfiguration = content
+        cell.accessoryType = .detailDisclosureButton
+        
         return cell
     }
 
