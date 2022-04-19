7f9b956e742407496a8a12fdbeb5bb10be755efe
Eric Grant <senatorericgrant@gmail.com>
2022-04-19 23:55:57 +0700

    feat: add the new load/save code

--- MyChecklists/AllListsViewController.swift
+++ MyChecklists/AllListsViewController.swift
@@ -18,23 +18,25 @@ class AllListsViewController: UITableViewController, ListDetailViewControllerDel
         tableView.register(UITableViewCell.self,
                            forCellReuseIdentifier: cellIdentifier)
         
-        var list = Checklist(name: "Birthdays")
-        lists.append(list)
-
-        list = Checklist(name: "Groceries")
-        lists.append(list)
-
-        list = Checklist(name: "Cool Apps")
-        lists.append(list)
-
-        list = Checklist(name: "To Do")
-        lists.append(list)
+        loadChecklists()
         
-        for list in lists {
-            let item = CheckListItem()
-            item.text = "Item for \(list.name)"
-            list.items.append(item)
-        }
+//        var list = Checklist(name: "Birthdays")
+//        lists.append(list)
+//
+//        list = Checklist(name: "Groceries")
+//        lists.append(list)
+//
+//        list = Checklist(name: "Cool Apps")
+//        lists.append(list)
+//
+//        list = Checklist(name: "To Do")
+//        lists.append(list)
+//
+//        for list in lists {
+//            let item = CheckListItem()
+//            item.text = "Item for \(list.name)"
+//            list.items.append(item)
+//        }
     }
     
     //MARK: - Navigation
@@ -143,5 +145,43 @@ class AllListsViewController: UITableViewController, ListDetailViewControllerDel
             }
         }
         navigationController?.popViewController(animated: true)
+    }
+    
+    //MARK: - Data Saving
+    func documentsDirectory() -> URL {
+        let paths = FileManager.default.urls(
+            for: .documentDirectory,
+            in: .userDomainMask)
+        return paths[0]
+    }
+    
+    func dataFilePath() -> URL {
+        return documentsDirectory().appendingPathComponent("Checklists.plist")
+    }
+    
+    func saveChecklists() {
+        let encoder = PropertyListEncoder()
+        do {
+            let data = try encoder.encode(lists)
+            try data.write(
+                to: dataFilePath(),
+                options: Data.WritingOptions.atomic)
+        } catch {
+            print("Error encoding list array: \(error.localizedDescription)")
+        }
+    }
+    
+    func loadChecklists() {
+        let path = dataFilePath()
+        if let data = try? Data(contentsOf: path) {
+            let decoder = PropertyListDecoder()
+            do {
+                lists = try decoder.decode(
+                    [Checklist].self,
+                    from: data)
+            } catch {
+                print("Error decoding list array: \(error.localizedDescription)")
+            }
+        }
     }
 }
--- MyChecklists/ChecklistViewController.swift
+++ MyChecklists/ChecklistViewController.swift
@@ -144,42 +144,5 @@ class ChecklistViewController: UITableViewController, ItemDetailViewControllerDe
             label.text = ""
         }
     }
-    
-//    func documentsDirectory() -> URL {
-//        let paths = FileManager.default.urls(
-//            for: .documentDirectory,
-//            in: .userDomainMask)
-//        return paths[0]
-//    }
-    
-//    func dataFilePath() -> URL {
-//        return documentsDirectory().appendingPathComponent("Checklists.plist")
-//    }
-    
-//    func saveChecklistsItems() {
-//        let encoder = PropertyListEncoder()
-//        do {
-//            let data = try encoder.encode(items)
-//            try data.write(
-//                to: dataFilePath(),
-//                options: Data.WritingOptions.atomic)
-//        } catch {
-//            print("Error encoding item array: \(error.localizedDescription)")
-//        }
-//    }
-    
-//    func loadChecklistItems() {
-//        let path = dataFilePath()
-//        if let data = try? Data(contentsOf: path) {
-//            let decoder = PropertyListDecoder()
-//            do {
-//                items = try decoder.decode(
-//                    [CheckListItem].self,
-//                    from: data)
-//            } catch {
-//                print("Error decoding item array: \(error.localizedDescription)")
-//            }
-//        }
-//    }
 }
 
