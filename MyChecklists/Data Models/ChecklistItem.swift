9bf4236c1fd1e9891703b0d94e0ea245c576c451
Eric Grant <senatorericgrant@gmail.com>
2022-04-24 23:26:53 +0700

    feat: associate to-do items with notifications

--- MyChecklists/Data Models/ChecklistItem.swift
+++ MyChecklists/Data Models/ChecklistItem.swift
@@ -10,10 +10,18 @@ import Foundation
 class CheckListItem: NSObject, Codable {
     var text = ""
     var checked = false
+    var dueData = Date()
+    var shouldRemind = false
+    var itemID = -1
     
     init(text: String, checked: Bool) {
         self.text = text
         self.checked = checked
         super.init()
+    }
+    
+    override init() {
+        super.init()
+        itemID = DataModel.nextChecklistItemID()
     }
 }
